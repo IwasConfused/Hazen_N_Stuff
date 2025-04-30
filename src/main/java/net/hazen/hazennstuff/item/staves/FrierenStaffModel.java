9c9e9db25fc337a33659d8dbf2b8d30685e16db0
Ace The King <solcat0414@gmail.com>
2025-04-30 15:44:01 -0400

    Changelog:
    Fixed Frieren Staff not showing up
    
    - Ace

--- src/main/java/net/hazen/hazennstuff/item/staves/FrierenStaffModel.java
+++ src/main/java/net/hazen/hazennstuff/item/staves/FrierenStaffModel.java
@@ -2,7 +2,6 @@ package net.hazen.hazennstuff.item.staves;
 
 import io.redspace.ironsspellbooks.IronsSpellbooks;
 import net.hazen.hazennstuff.HazenNStuff;
-import net.hazen.hazennstuff.item.staves.FrierenStaffItem;
 import net.minecraft.resources.ResourceLocation;
 import software.bernie.geckolib.model.DefaultedItemGeoModel;
 
